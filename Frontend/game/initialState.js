d900f517906c149a6dc594567961f6636a0a5a79
jamalsnow69-glitch <jamalsnow69@gmail.com>
2026-05-31 16:26:07 -0700

    Update initialState.js

--- Frontend/game/initialState.js
+++ Frontend/game/initialState.js
@@ -1,1 +1,25 @@
-
+export const initialGameState = {
+  day: 1,
+  location: "Ashvale Outskirts",
+  health: 100,
+  hunger: 20,
+  thirst: 20,
+  fatigue: 10,
+  morale: 60,
+  faction: "Unaffiliated",
+  inventory: {
+    scrap: 3,
+    canned_food: 2,
+    water_bottle: 2,
+    old_knife: 1
+  },
+  quests: {
+    waterPump: false,
+    signalFound: false,
+    militiaDeal: false
+  },
+  log: [
+    "Dead Grid initialized.",
+    "Fifteen years after the power died."
+  ]
+};
